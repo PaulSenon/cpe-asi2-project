65d479e8cb0b27d8777dfffd166532d653786283
clementgillet <gillet_clement@yahoo.fr>
2019-12-06 17:37:42 +0100

    new view

--- game-server/app.js
+++ game-server/app.js
@@ -6,26 +6,77 @@ let brocker;
 let logger = new Logger();
 
 (async () => {
-    try{
-        brocker = await new Brocker({
+
+    // EXPRESS
+    app.use(express.static(CONFIG.publicDir))
+    app.get('/', function (req, res) {
+        res.sendfile('index.html');
+    });
+    server.listen(CONFIG.port, () => console.log(`Listening on port ${CONFIG.port}!`));
+
+    // BROCKER
+    try {
+        const brocker = await new Brocker({
             host: CONFIG.brocker.host,
             port: CONFIG.brocker.port,
         });
-        const sendToLoggerCb = (message => {
-            brocker.sendMessage(CONFIG.brocker.queues.sendLogs, message);
-        })
-        logger = new Logger(CONFIG.appName, sendToLoggerCb);
-        logger.info(`socket.io server UP at port ${CONFIG.port}`);
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
-    
-    io.on('connect', socket => {
-        logger.info(`new user connected: ${socket.id}`);
-        socket.on('message', data => {
-            logger.info(data);
-            logger.critical("lol");
-        });
-    });
+
+    // SOCKET
+    const socket = await new Socket(server);
+
+    // brocker.onMessage(CONFIG.brocker.queues.receiveFrom, message => {
+    //     console.log('brocker on message', message);
+    //     const data = JSON.parse(message);
+    //     data['processedByNode'] = true;
+    //     socket.emit('serverResponse', data);
+    // });
+    // socket.on('message', data => {
+    //     console.log('superçamarche', data);
+    // });
+
+    socket.on('message', data => {
+        console.log('io.userInfo', data)
+        try {
+            try {
+                const data = JSON.stringify(data);
+            } catch (e) { }
+            // brocker.sendMessage(CONFIG.brocker.queues.sendLogs, data);
+        } catch (e) {
+            console.error(e);
+        }
+    }).catch(e => console.log);
+
+    // socket.on('userInfo', data => {
+    //     console.log('io.userInfo', data)
+    //     try{
+    //         try {
+    //             const data = JSON.parse(data);
+    //         }catch(e){}
+    //         const { nom, prenom } = data;
+    //         // brocker.sendMessage(CONFIG.brocker.queues.sendTo, JSON.stringify({nom, prenom}));
+    //     }catch(e){
+    //         console.error(e);
+    //     }
+    // });
+
+    // // SOCKET.IO
+    // ioServer.on('connection', function(socket){
+    //   console.log('a user connected');
+    //   socket.on('userInfo', function(data) {
+    //     console.log('io.userInfo', data)
+    //     try{
+    //         const { nom, prenom } = JSON.parse(data);
+    //         brocker.sendMessage(CONFIG.brocker.queues.sendTo, JSON.stringify({nom, prenom}));
+
+    //     }catch(e){
+    //         console.error(e);
+    //     }
+    //     // socket.emit('myEvent2', data);
+    //   });
+    // });
+
 
 })();
